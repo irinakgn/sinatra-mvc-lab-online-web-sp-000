9b0e306e2d8fc45479800fb1a962e15cd78fd788
Irina Kagan <irinagamb@gmail.com>
2019-07-31 22:30:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require_relative 'config/environment'
+require_relative 'model/piglatinizer.rb'
 
 class App < Sinatra::Base
 
   get '/' do
-    erb : user_input
+    erb :user_input
   end
 
 
   post '/submit-form' do
+    @pigLatin = PigLatinizer.new(params[:name])
   end
 end
